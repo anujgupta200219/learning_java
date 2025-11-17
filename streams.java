e68fb8bee38e44092a38cc74fbb252345b46772a
Anuj <113493490+anujgupta200219@users.noreply.github.com>
2025-11-17 18:13:54 +0530

    Update streams.java

--- streams.java
+++ streams.java
@@ -10,23 +10,38 @@ public class streams {
         li.add(3);
         li.add(9);
         li.add(2);
-        Stream<Integer> stream = li.stream();
+        Stream<Integer> stream = li.stream(); //creating stream from list
 
         Integer arr[]={6,3,5,8,9,2};
-        Stream<Integer> stream1 = Arrays.stream(arr);
+        Stream<Integer> stream1 = Arrays.stream(arr); //creating steam from array 
 
-        List<Integer> filter = li.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        System.out.println(filter);
+        List<Integer> filter = li.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());  //using filter
+        System.out.println("x%2 -> "+filter);
 
-        List<Integer> map = li.stream().map(x -> x/2)
+        List<Integer> map = li.stream()
+                .map(x -> x/2) //using map
+                .distinct()    //provide distinct elements
+                .sorted(Collections.reverseOrder()) // sort the list and it want object of comparator
+                .limit(2)  //limit of the scanning
+                .skip(1)   // skip the elements
+                .collect(Collectors.toList()); //convert stream to list
+        System.out.println("x/2 -> "+map);
+
+        List<Integer> collect = Stream
+                .iterate(0, x -> x + 1)
+                .limit(10)
+                .peek(x-> System.out.print(x+" "))  //perform operation in between the urls
+                .collect(Collectors.toList()); 
+        System.out.println();
+        System.out.println("x+1 -> "+collect);
+
+        System.out.println("min -> "+li.stream()
+                .map(x->x)
+                .min((a,b)->a-b));  //provide min of the elements
+
+        System.out.println("count -> "+li.stream()
+                .map(x->x)
                 .distinct()
-                .sorted(Collections.reverseOrder())
-                .limit(2)
-                .skip(1)
-                .collect(Collectors.toList());
-        System.out.println(map);
-
-        List<Integer> collect = Stream.iterate(0, x -> x + 1).limit(10).collect(Collectors.toList());
-        System.out.println(collect);
+                .count());  //provides count of the elements.
     }
 }
